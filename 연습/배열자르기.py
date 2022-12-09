8d581b079cd2a6140e38bd81d9dea622e935c1af
k2koo <114331509+k2koo@users.noreply.github.com>
2022-12-09 19:09:42 +0900

    Update 배열자르기.py

--- 연습/배열자르기.py
+++ 연습/배열자르기.py
@@ -3,3 +3,9 @@
 def solution(numbers, num1, num2):
     answer = numbers[num1:num2+1]
     return answer
+
+def solution(numbers, num1, num2):
+    answer=[]
+    for i in range(num1,num2+1):
+        answer.append(numbers[i])
+    return answer
