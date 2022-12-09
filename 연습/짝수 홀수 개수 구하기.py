bf88cf5c2042990955daa0d41bc724186ddf1cdb
k2koo <114331509+k2koo@users.noreply.github.com>
2022-12-09 19:24:31 +0900

    Create 짝수 홀수 개수 구하기.py

---
+++ 연습/짝수 홀수 개수 구하기.py
@@ -0,0 +1,20 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/120824
+
+def solution(num_list):
+    answer = [0,0]
+    for i in num_list:
+        if i % 2 == 0:
+            answer[0] += 1
+        else:
+            answer[1] += 1
+            
+        
+    return answer
+
+def solution(num_list):
+    answer = [0,0]
+    for n in num_list:
+        answer[n%2]+=1
+    return answer
+  
+  
