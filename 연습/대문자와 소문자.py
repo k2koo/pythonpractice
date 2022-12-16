a536c0f44c4fabef870b8e19bf5fe8cab350db21
k2koo <114331509+k2koo@users.noreply.github.com>
2022-12-16 15:16:51 +0900

    Create 대문자와 소문자.py

---
+++ 연습/대문자와 소문자.py
@@ -0,0 +1,15 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/120893
+
+def solution(my_string):
+    answer = my_string.swapcase()
+    return answer
+  
+  def solution(my_string):
+    answer = ''
+
+    for i in my_string:
+        if i.isupper():
+            answer+=i.lower()
+        else:
+            answer+=i.upper()
+    return answer
