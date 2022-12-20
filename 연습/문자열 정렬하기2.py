10eadd34084c720557f7c1ecf68880fd23414925
k2koo <114331509+k2koo@users.noreply.github.com>
2022-12-20 22:06:38 +0900

    Create 문자열 정렬하기2.py

---
+++ 연습/문자열 정렬하기2.py
@@ -0,0 +1,9 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/120911
+
+def solution(my_string):
+    answer = ''
+    a = my_string.lower()
+    answer = ''.join(sorted(a))
+    return answer
+  
+  return ''.join(sorted(my_string.lower()))
